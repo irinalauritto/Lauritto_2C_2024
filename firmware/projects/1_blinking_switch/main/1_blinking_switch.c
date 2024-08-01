44d26bbb86bae808ca153f546b856bf8c0ed492d
irinalauritto <irina.lauritto@ingenieria.uner.edu.ar>
2024-08-01 11:23:34 -0300

    Se sube actividad 1, ejercicios 1 y 2.

--- firmware/projects/1_blinking_switch/main/1_blinking_switch.c
+++ firmware/projects/1_blinking_switch/main/1_blinking_switch.c
@@ -23,7 +23,9 @@
 #include "led.h"
 #include "switch.h"
 /*==================[macros and definitions]=================================*/
-#define CONFIG_BLINK_PERIOD 1000
+#define CONFIG_BLINK_PERIOD_LED1 500
+#define CONFIG_BLINK_PERIOD_LED2 800
+#define CONFIG_BLINK_PERIOD_LED3 300
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -35,15 +37,32 @@ void app_main(void){
 	SwitchesInit();
     while(1)    {
     	teclas  = SwitchesRead();
+		printf("tecla%d\r\n", teclas);
     	switch(teclas){
     		case SWITCH_1:
     			LedToggle(LED_1);
+				LedOff(LED_2 | LED_3);
+				vTaskDelay(CONFIG_BLINK_PERIOD_LED1 / portTICK_PERIOD_MS);
     		break;
+
     		case SWITCH_2:
     			LedToggle(LED_2);
+				LedOff(LED_1 | LED_3);
+				vTaskDelay(CONFIG_BLINK_PERIOD_LED2 / portTICK_PERIOD_MS);
+    		break;
+
+			case SWITCH_1|SWITCH_2:
+    			LedToggle(LED_3);
+				LedOff(LED_1 | LED_2);
+				vTaskDelay(CONFIG_BLINK_PERIOD_LED3 / portTICK_PERIOD_MS);
+    		break;
+
+			//Debería apagar los LEDs, al igual que las lineas 45, 51, y 57. No lo hace.
+			case (~SWITCH_1)|(~SWITCH_2):
+				LedOff(LED_1 & LED_2 & LED_3);
     		break;
     	}
-	    LedToggle(LED_3);
-		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	    //LedToggle(LED_3);
+		//vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
 	}
 }
